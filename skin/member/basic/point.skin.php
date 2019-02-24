f7d940efa8f566b7d646ce1dd9317ef6cf8c1d0e
shinkeonkim <singun11@naver.com>
2019-02-24 16:40:03 +0900

    table로 변경중

--- skin/member/basic/point.skin.php
+++ skin/member/basic/point.skin.php
@@ -8,6 +8,98 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 <div id="point" class="new_win">
     <h1 id="win_title"><i class="fa fa-database" aria-hidden="true"></i> <?php echo $g5['title'] ?></h1>
 
+    <?php
+            $sum_point1 = $sum_point2 = $sum_point3 = 0;
+
+            $sql = " select *
+                        {$sql_common}
+                        {$sql_order}
+                        limit {$from_record}, {$rows} ";
+            $result = sql_query($sql);
+            for ($i=0; $row=sql_fetch_array($result); $i++) {
+                $point1 = $point2 = 0;
+                if ($row['po_point'] > 0) {
+                    $point1 = '+' .number_format($row['po_point']);
+                    $sum_point1 += $row['po_point'];
+                } else {
+                    $point2 = number_format($row['po_point']);
+                    $sum_point2 += $row['po_point'];
+                }
+
+                $po_content = $row['po_content'];
+
+                $expr = '';
+                if($row['po_expired'] == 1)
+                    $expr = ' txt_expired';
+    ?>
+    <table>
+        <thead>
+            <tr>
+                <th> 전체  </th> <th> 내용 </th> <th> 만료 </th> <th> 지급 </th> <th> 사용 </th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+        </tbody>
+
+        <tfoot>
+            <tr> 
+                <td> 보유포인트 <td>
+                <td> <i class="fa fa-database" aria-hidden="true"></i> <?php echo number_format($member['mb_point']); ?> <td>
+            </tr>
+            <tr>
+                <td> 소계 </td>
+                <td> <?php echo $sum_point1; ?> <td>
+                <td> <?php echo $sum_point2; ?></span></td>
+            </tr>
+        </tfoot>
+    </table>
+    <?php
+            }
+
+            if ($i == 0)
+                echo '<li class="empty_li">자료가 없습니다.</li>';
+            else {
+                if ($sum_point1 > 0)
+                    $sum_point1 = "+" . number_format($sum_point1);
+                $sum_point2 = number_format($sum_point2);
+            }
+            ?>
+    <div class="new_win_con list_01">
+
+
+        <ul>
+            
+            
+            <li>
+                <div class="point_top">
+                    <span class="point_tit"><?php echo $po_content; ?></span>
+                    <span class="point_num"><?php if ($point1) echo $point1; else echo $point2; ?></span>
+
+                </div>
+                <span class="point_date1"><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo $row['po_datetime']; ?></span>
+                <span class="point_date<?php echo $expr; ?>">
+                    <?php if ($row['po_expired'] == 1) { ?>
+                    만료 <?php echo substr(str_replace('-', '', $row['po_expire_date']), 2); ?>
+                    <?php } else echo $row['po_expire_date'] == '9999-12-31' ? '&nbsp;' : $row['po_expire_date']; ?>
+                </span>
+            </li>
+        </ul>
+
+    </div>
+
+    <?php echo get_paging(G5_IS_MOBILE ? $config['cf_mobile_pages'] : $config['cf_write_pages'], $page, $total_page, $_SERVER['SCRIPT_NAME'].'?'.$qstr.'&amp;page='); ?>
+
+    <button type="button" onclick="javascript:window.close();" class="btn_close">창닫기</button>
+</div>
+
+
+
+<!--
+<div id="point" class="new_win">
+    <h1 id="win_title"><i class="fa fa-database" aria-hidden="true"></i> <?php echo $g5['title'] ?></h1>
+
     <div class="new_win_con list_01">
         
         <ul>
@@ -77,3 +169,5 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 
     <button type="button" onclick="javascript:window.close();" class="btn_close">창닫기</button>
 </div>
+
+-->
