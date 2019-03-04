f93b3de84ec174d9f79dcf1bb4897492efdb99fb
shinkeonkim <singun11@naver.com>
2019-03-04 09:13:10 +0900

    한민고등학교 기수, 1학년 반 쿼리 추가

--- bbs/member_certification.php
+++ bbs/member_certification.php
@@ -10,6 +10,30 @@ $g5['title'] = '한민고 학생 인증';
 include_once(G5_PATH.'/head.php');
 
 //include_once($member_skin_path.'/member_confirm.skin.php');
+?>
 
+<div class="page_certification">
+    <form action="memeber_certification_update.php" method="post">
+        <div class="content_certification">
+            <p> 한민고등학교 몇 기 이신지 작성해주세요. </p>
+            <input type="text" name="mc_number" placeholder="(숫자만 입력해주세요)">
+            <p> 한민고등학교 1학년 몇 반이신지 작성해주세요. </p>
+            <input type="text" name="mc_fclass" placeholder="(숫자만 입력해주세요)"> 
+            
+            <p> 한민고등학교 재학생, 졸업생을 인증하는 질문입니다. </p>
+            <?php 
+                // DB 끌어와야됨.
+                
+
+            ?>
+            
+
+
+        </div>
+    </form>
+
+</div>
+
+<?php
 include_once(G5_PATH.'/tail.php');
 ?>
