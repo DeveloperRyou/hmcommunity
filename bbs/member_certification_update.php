24a79a33b3514afab0bc7fe1df8a8ec19667bf71
Son-GyeongSik <47748085+Son-GyeongSik@users.noreply.github.com>
2019-03-12 17:27:04 +0900

    한민인증 답안 비교 php 작성
    1. 유저가 제출한 답안과 db에 적힌 답안 비교하는 php
    2. 맞으면 맞다는 알림 띄우고 홈화면으로 이동
    3. 틀리면 틀리다는 알림 띄우고 다시 원래 화면으로 이동
    
    <향후 수정사항>
    맞았을 때 유저가 입력한 기수, 반 데이터를 db에 적용하는 부분을 작성해야함

--- bbs/member_certification_update.php
+++ bbs/member_certification_update.php
@@ -1,11 +1,26 @@
 <?php
 include_once('./_common.php');
-
 if (!$is_member)//미가입자 접근 불가 설정
     alert('회원만 이용하실 수 있습니다.', './login.php');
 
 
 $g5['title'] = '한민고 학생 인증';
-//스킨 파일에서 form 태그를 이용해 POST방식으로 넘긴 변수와 답 DB를 대조하여 맞으면 회원 권한 3, 틀리면 스킨페이지로 이동
 
+?>
+<?php
+//답안 DB 접근 및 값 비교
+$sql = "SELECT * FROM g5_member_certification";
+$result = sql_query($sql);
+for ($i=0; $row=sql_fetch_array($result); $i++){
+    $question[$i]=$row;
+}
+for ($j=0; $j<=5; $j++)
+{
+	$user_a=$_POST['mc_A'.$j];
+	$db=$question[$_POST['mc_Q'.$j]];
+	$re=strcmp($user_a,$db['qa_answer']);
+	if($re!=0)
+		alert('틀렸습니다 ㅠㅠ 다시 시도해주세요','./member_certification.php');
+}
+alert('인증에 성공했습니다!',G5_URL);
 ?>
