facec1bcc715527b0ca771aedf9c3ccb9abb8bb0
ryou73 <ryou73@github.com>
2019-08-01 20:21:23 +0900

    김승민 관리자 추가, 주석 수정, 한아름 바로가기 버튼 추가

--- hanmincamp/head_pc.php
+++ hanmincamp/head_pc.php
@@ -150,6 +150,9 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
                 if ($i == 0) {  ?>
                     <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
                 <?php } ?>
+                <li class="gnb_1dli" style="float: right">
+                  <a href="<?php echo G5_URL ?>?hanmincamp=false" class="gnb_1da">한아름 바로가기 </a>
+                </li>
             </ul>
             <div id="gnb_all">
                 <h2>전체메뉴</h2>
