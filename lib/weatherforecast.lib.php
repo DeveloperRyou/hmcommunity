d7f059a4088149a5fc88dd70cd8d8961bddc50d2
ryou73 <ryou73@github.com>
2019-11-16 14:52:46 +0900

    DB 추가 및 API를 이용하여 날씨 업데이트 기능 완료

--- lib/weatherforecast.lib.php
+++ lib/weatherforecast.lib.php
@@ -1,65 +1,68 @@
 <?php
 //if (!defined('_GNUBOARD_')) exit;
 
-if(true){
-  $jsonfile = file_get_contents("http://api.openweathermap.org/data/2.5/forecast?q=Munemi,KR&units=metric&appid=5c331b6c10405ea2b5c6c670a1814c62");
-  $jsondata = json_decode($jsonfile,true);
 
-  date_default_timezone_set("Asia/Seoul");
-  $timestamp = new DateTime();
-  $today = substr($timestamp->format("Y-m-d H:i:s"), 0, 10);
-  $timestamp = strtotime(today." +1 days +6 hours");
-  $tomorrow = date("Y-m-d H:i:s", $timestamp);
-  foreach ($jsondata['list'] as $day => $value) {
-    $date = $value['dt_txt'];
-    if(strcmp($date,$tomorrow)==0){
-      //데이터 베이스 입력
-
-
-
-
-      break;
-    }
-  }
-
-  echo "</br>DONE";
-}/*
 // 방문자수 출력
 function weather($skin_dir='basic',$days=1)
 {
+    date_default_timezone_set("Asia/Seoul");
+    $timestamp = new DateTime();
+    $today = substr($timestamp->format("Y-m-d H:i:s"), 0, 10);
+    $timestamp = strtotime(today." +1 days +6 hours");
+    $tomorrow = date("Y-m-d H:i:s", $timestamp);
+
     global $config, $g5;
 
-    $sql = "SELECT mb_nick, mb_point FROM `g5_member` WHERE mb_level < 10 ORDER BY `mb_point` DESC LIMIT {$num_person}";
+    $sql = "SELECT api_txt1,api_txt2,api_txt3 FROM `g5_apis` WHERE api_id='weatherforecast' ";
     $result = sql_query($sql);
     for ($i=0; $row=sql_fetch_array($result); $i++) {
-        $mb_nick[$i] = $row['mb_nick'];
-        $mb_point[$i] = $row['mb_point'];
+        $db_today = $row['api_txt1'];
+        $main = $row['api_txt2'];
+        $description = $row['api_txt2'];
+    }
+
+    if(strcmp($today,$db_today)==1){
+      $jsonfile = file_get_contents("http://api.openweathermap.org/data/2.5/forecast?q=Munemi,KR&units=metric&appid=5c331b6c10405ea2b5c6c670a1814c62");
+      $jsondata = json_decode($jsonfile,true);
+
+      foreach ($jsondata['list'] as $day => $value) {
+        $date = $value['dt_txt'];
+        if(strcmp($date,$tomorrow)==0){
+          $main = $value['weather'][0]['main'];
+          $description = $value['weather'][0]['description'];
+          $sql = " update g5_apis
+                      set api_txt1 = '{$today}', api_txt2 = '{$main}', api_txt3 = '{$description}'
+                    where api_id   = 'weatherforecast' ";
+          sql_query($sql);
+          break;
+        }
+      }
     }
 
 
     if(preg_match('#^theme/(.+)$#', $skin_dir, $match)) {
         if (G5_IS_MOBILE) {
-            $pointrank_skin_path = G5_THEME_MOBILE_PATH.'/'.G5_SKIN_DIR.'/pointrank/'.$match[1];
-            if(!is_dir($pointrank_skin_path))
-                $pointrank_skin_path = G5_THEME_PATH.'/'.G5_SKIN_DIR.'/pointrank/'.$match[1];
-            $pointrank_skin_url = str_replace(G5_PATH, G5_URL, $pointrank_skin_path);
+            $weather_skin_path = G5_THEME_MOBILE_PATH.'/'.G5_SKIN_DIR.'/weather/'.$match[1];
+            if(!is_dir($weather_skin_path))
+                $weather_skin_path = G5_THEME_PATH.'/'.G5_SKIN_DIR.'/weather/'.$match[1];
+            $weather_skin_url = str_replace(G5_PATH, G5_URL, $weather_skin_path);
         } else {
-            $pointrank_skin_path = G5_THEME_PATH.'/'.G5_SKIN_DIR.'/pointrank/'.$match[1];
-            $pointrank_skin_url = str_replace(G5_PATH, G5_URL, $pointrank_skin_path);
+            $weather_skin_path = G5_THEME_PATH.'/'.G5_SKIN_DIR.'/weather/'.$match[1];
+            $weather_skin_url = str_replace(G5_PATH, G5_URL, $weather_skin_path);
         }
         $skin_dir = $match[1];
     } else {
         if(G5_IS_MOBILE) {
-            $pointrank_skin_path = G5_MOBILE_PATH.'/'.G5_SKIN_DIR.'/pointrank/'.$skin_dir;
-            $pointrank_skin_url = G5_MOBILE_URL.'/'.G5_SKIN_DIR.'/pointrank/'.$skin_dir;
+            $weather_skin_path = G5_MOBILE_PATH.'/'.G5_SKIN_DIR.'/weather/'.$skin_dir;
+            $weather_skin_url = G5_MOBILE_URL.'/'.G5_SKIN_DIR.'/weather/'.$skin_dir;
         } else {
-            $pointrank_skin_path = G5_SKIN_PATH.'/pointrank/'.$skin_dir;
-            $pointrank_skin_url = G5_SKIN_URL.'/pointrank/'.$skin_dir;
+            $weather_skin_path = G5_SKIN_PATH.'/weather/'.$skin_dir;
+            $weather_skin_url = G5_SKIN_URL.'/weather/'.$skin_dir;
         }
     }
 
     ob_start();
-    include_once ($pointrank_skin_path.'/pointrank.skin.php');
+    include_once ($weather_skin_path.'/weather.skin.php');
     $content = ob_get_contents();
     ob_end_clean();
 
