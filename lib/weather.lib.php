74bdc3162a32a4ca2414854b700050d9de4ec851
ryou73 <ryou73@github.com>
2019-12-05 14:14:39 +0900

    오늘, 내일, 모래 날씨정보 API에서 얻어서 DB저장 후 출력

--- lib/weather.lib.php
+++ lib/weather.lib.php
@@ -1,41 +1,79 @@
 <?php
-if (!defined('_GNUBOARD_')) exit;
+//if (!defined('_GNUBOARD_')) exit;
 
 // 방문자수 출력
 function weather($skin_dir='basic')
 {
+    global $config, $g5;
+
+    //init
+    for($i=1;$i<=3;$i++){
+      $sql = "SELECT api_id FROM `g5_apis` WHERE api_id='weather_{$i}' ";
+      $row = sql_fetch($sql);
+      if($row['api_id']==False) {
+        $sql = "INSERT INTO `g5_apis` SET api_id = 'weather_{$i}', api_txt1='None' ";
+        $row = sql_fetch($sql);
+      }
+    }
+
     date_default_timezone_set("Asia/Seoul");
     $timestamp = new DateTime();
     $today = substr($timestamp->format("Y-m-d H:i:s"), 0, 10);
-    $timestamp = strtotime(today." +1 days +6 hours");
-    $tomorrow = date("Y-m-d H:i:s", $timestamp);
+    $day1 = date("Y-m-d H:i:s", strtotime($today." +6 hours"));
+    $day2 = date("Y-m-d H:i:s", strtotime($day1." +1 days"));
+    $day3 = date("Y-m-d H:i:s", strtotime($day2." +1 days"));
 
-    global $config, $g5;
-
-    $sql = "SELECT api_txt1,api_txt2,api_txt3 FROM `g5_apis` WHERE api_id='weather' ";
+    $sql = "SELECT api_txt1 FROM `g5_apis` WHERE api_id='weather_1' ";
     $row = sql_fetch($sql);
         $db_today = $row['api_txt1'];
-        $main = $row['api_txt2'];
-        $description = $row['api_txt3'];
 
-    if(strcmp($today,$db_today)==1){
+    if(strcmp($day1,$db_today)!=0){
       $jsonfile = file_get_contents("http://api.openweathermap.org/data/2.5/forecast?q=Munemi,KR&units=metric&appid=5c331b6c10405ea2b5c6c670a1814c62");
       $jsondata = json_decode($jsonfile,true);
 
       foreach ($jsondata['list'] as $day => $value) {
         $date = $value['dt_txt'];
-        if(strcmp($date,$tomorrow)==0){
-          $main = $value['weather'][0]['main'];
-          $description = $value['weather'][0]['description'];
+        $main = $value['weather'][0]['main'];
+        $description = $value['weather'][0]['description'];
+
+        if(strcmp($date,$day1)==0){
           $sql = " update g5_apis
-                      set api_txt1 = '{$today}', api_txt2 = '{$main}', api_txt3 = '{$description}'
-                    where api_id   = 'weather' ";
+                      set api_txt1 = '{$day1}', api_txt2 = '{$main}', api_txt3 = '{$description}'
+                    where api_id   = 'weather_1' ";
           sql_fetch($sql);
-          break;
+        }
+
+        if(strcmp($date,$day2)==0){
+          $sql = " update g5_apis
+                      set api_txt1 = '{$day2}', api_txt2 = '{$main}', api_txt3 = '{$description}'
+                    where api_id   = 'weather_2' ";
+          sql_fetch($sql);
+        }
+
+        if(strcmp($date,$day3)==0){
+          $sql = " update g5_apis
+                      set api_txt1 = '{$day3}', api_txt2 = '{$main}', api_txt3 = '{$description}'
+                    where api_id   = 'weather_3' ";
+          sql_fetch($sql);
         }
       }
+
     }
 
+    $sql = "SELECT * FROM `g5_apis` WHERE api_id='weather_1' ";
+    $row = sql_fetch($sql);
+    $main[1] = $row['api_txt2'];
+    $description[1] = $row['api_txt3'];
+
+    $sql = "SELECT * FROM `g5_apis` WHERE api_id='weather_2' ";
+    $row = sql_fetch($sql);
+    $main[2] = $row['api_txt2'];
+    $description[2] = $row['api_txt3'];
+
+    $sql = "SELECT * FROM `g5_apis` WHERE api_id='weather_3' ";
+    $row = sql_fetch($sql);
+    $main[3] = $row['api_txt2'];
+    $description[3] = $row['api_txt3'];
 
     if(preg_match('#^theme/(.+)$#', $skin_dir, $match)) {
         if (G5_IS_MOBILE) {
--- theme/basic/skin/weather/basic/weather.skin.php
+++ theme/basic/skin/weather/basic/weather.skin.php
@@ -1,30 +1,44 @@
 <?php
-if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
+//if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 
 global $is_admin;
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$weather_skin_url.'/style.css">', 0);
 add_stylesheet('<link rel="stylesheet" href="'.$weather_skin_url.'/weather-icons.css">', 0);
+
+$icon['Thunderstorm'] ='wi wi-lightning';
+$icon['Drizzle'] ='wi wi-fog';
+$icon['Snow'] ='wi wi-snow';
+$icon['Clear'] ='wi wi-day-sunny';
+$icon['Clouds'] ='wi wi-cloudy';
+$icon['Rain'] ='wi wi-rain';
+
+$date[1] = '오늘';
+$date[2] = '내일';
+$date[3] = '모레';
+
 ?>
 
 <!-- 포인트 랭킹 시작 { -->
 <section id="weather">
       <div>
         <!-- 개발 시 https://erikflowers.github.io/weather-icons/ 참고하기-->
-        <div>내일의 날씨</div>
         <div class="">
-          <i class="wi wi-day-sunny"></i>
-          <?php echo $main;?>
+          <?php
+          for($i=1;$i<=3;$i++){?>
+            <div class="" style="width:32%; display:inline-block;">
+              <p><?php echo $date[$i];?>의 날씨</p>
+              <div class="">
+                <i class="<?php echo $icon[$main[$i]]; ?>"></i>
+                <?php echo $main[$i];?>
+              </div>
+              <p><?php echo $description[$i]; ?><p>
+            </div>
+          <?php }?>
         </div>
-        <div class="">
-          <i class="wi wi-rain"></i>
-          <?php echo $description ?>
-        </div>
-        <div class="">
-          기우제 지내기
-          <button type="button" name="button"></button> <!--포인트를 써서 기우제를 지내보자!-->
-        </div>
+
+        <button type="button" name="button"><p>기우제 지내기</p></button> <!--포인트를 써서 기우제를 지내보자!-->
       </div>
 
 </section>
